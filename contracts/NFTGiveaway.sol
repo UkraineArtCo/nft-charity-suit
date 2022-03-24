252b4665a278b95ac54328be3e947167e1fb18f4
maryjanyes <maryjanyes@gmail.com>
2022-03-24 23:46:18 +0200

    implement transfer to charity wallet + start with loyalty

--- contracts/NFTGiveaway.sol
+++ contracts/NFTGiveaway.sol
@@ -6,29 +6,33 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 import "./interfaces/Struct.sol";
 
 import {StringUtils} from "./libraries/StringUtils.sol";
 import {Base64} from "./libraries/Base64.sol";
 
-
-contract SaveUkraineNFT is ERC721URIStorage, Ownable, Struct {
+// ERC721Royalty
+contract UkraineArtCoNFT is ERC721URIStorage, Ownable, Struct {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     address public firstBayer;
-    address public copyOfOwner;
+    address payable public charityDestinationWallet;
+    mapping(uint => MarketItem) public marketItems;
 
-    constructor() ERC721("SaveUkraineNFT", "NFT") {
-        copyOfOwner = msg.sender;
+    constructor() ERC721("UkraineArtCo", "NFT") {
+        charityDestinationWallet = payable(0x2c3108dA62edd2835F4B5E471Af6401f0990Dadc);
     }
 
-    function calculateSoldRate() public pure returns(uint256) {
-        return 0;
+    function setTokenRoyalty(
+        uint256 tokenId,
+        address recipient,
+        uint96 fraction
+    ) public {
+        // _setTokenRoyalty(tokenId, recipient, fraction);
     }
 
     function mintNFT(string memory tokenURI) public payable returns (uint256) {
-
         require(msg.value >= 1, "Minimum 1 Matic is not met");
 
         if (_tokenIds.current() == 0) {
@@ -40,7 +44,7 @@ contract SaveUkraineNFT is ERC721URIStorage, Ownable, Struct {
             bytes(
                 string(
                     abi.encodePacked(
-                        '{"description": "SaveUkraineNFT donation", "image": "',
+                        '{"description": "UkraineArtCo donation", "image": "',
                         tokenURI,
                         '"}'
                     )
@@ -48,14 +52,16 @@ contract SaveUkraineNFT is ERC721URIStorage, Ownable, Struct {
             )
         );
 
-        string memory finalTokenUri = string( abi.encodePacked("data:application/json;base64,", json));
+        string memory finalTokenUri = string(abi.encodePacked("data:application/json;base64,", json));
 
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, finalTokenUri);
+        marketItems[newItemId] = MarketItem(newItemId, msg.value);
+        setTokenRoyalty(newItemId, charityDestinationWallet, 50);
         emit MarketItemSold(newItemId, msg.sender);
-
+        charityDestinationWallet.transfer(msg.value);
 
         return newItemId;
     }
