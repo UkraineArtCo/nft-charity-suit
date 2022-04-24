84034e8da17fbb0b324e63e32329c3280c5d1b51
antagor07 <rovor7@gmail.com>
2022-04-24 16:10:47 -0400

    reduce min to 0.01

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -38,7 +38,7 @@ const App = () => {
 
 	// Add some state data propertie
 	const [name, setName] = useState('');
-	const [amount, setAmount] = useState(0.07);
+	const [amount, setAmount] = useState(0.01);
   	const [currentAccount, setCurrentAccount] = useState('');
 	const [network, setNetwork] = useState('');
 	const [bg, setBg] = useState('');
@@ -281,7 +281,7 @@ const App = () => {
 
 				console.log("network", network)
 
-				if (Number(amount) >= 0.07) {
+				if (Number(amount) >= 0.01) {
 
 					const contract = new ethers.Contract(CONTRACT_ADDRESS, ethContractAbi.abi, signer);			
 					const NFTnameId = Math.floor(Math.random()*150) + 1;
@@ -312,8 +312,8 @@ const App = () => {
 
 				} else {
 
-					setErrorMessage('Minimum ETH is 0.07');
-					console.log("Minimum ETH is 0.07");
+					setErrorMessage('Minimum ETH is 0.01');
+					console.log("Minimum ETH is 0.01");
 
 				}
 
@@ -510,7 +510,7 @@ const App = () => {
 				</div>
 				<div className="center">
 						<div className="title"><p className="blue">Ukraine</p><p className="yellow">Art</p><p className="yellow">Collective</p></div>
-						<p className="subtitle">Harnessing global creative power to support the Ukrainian people. When you donate, you get an NFT created by Ukrainian artists. We appreciate donation of any amount, with recommended minimum of 0.07 ETH</p>
+						<p className="subtitle">Harnessing global creative power to support the Ukrainian people. When you donate, you get an NFT created by Ukrainian artists. We appreciate donation of any amount, with recommended minimum of 0.01 ETH</p>
 						{/* Hide the connect button if currentAccount isn't empty*/}
 						{!currentAccount && renderNotConnectedContainer()}
 						{currentAccount && renderWhenWalletConnected()}
